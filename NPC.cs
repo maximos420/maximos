c34347585e073a3771e4940134ec9c33ec9bd8fb
maximos420 <139778889+maximos420@users.noreply.github.com>
2023-07-19 16:28:38 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //NPC Health 
+   public  int health = 5;
+    //NPC Level
+   public int level = 1;
+    //NPC Speed 
+   public float Speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
